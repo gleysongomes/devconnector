c1d0ea1ad8b6f4829722b4ba80ebff0d873c8884
Gleyson Gomes <fgleysondasilva@gmail.com>
2019-01-25 21:08:45 -0200

    Funcionalidades de post e comentário.

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,5 +1,5 @@
 import {
-    GET_ERRORS
+    GET_ERRORS, CLEAR_ERRORS
 } from '../actions/types';
 
 const initialState = {};
@@ -8,6 +8,8 @@ export default function (state = initialState, action) {
     switch (action.type) {
         case GET_ERRORS:
             return action.payload;
+        case CLEAR_ERRORS:
+            return {};
         default:
             return state;
     }
